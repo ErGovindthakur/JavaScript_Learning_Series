93dc9e50cbb7fe78352990da4243413704f6f1e0
ErGovindthakur <ergovindthakur@gmail.com>
2023-12-30 16:25:26 +0530

    JavaScript learning series in progress

---
+++ Chapter-03/Chapter_3.js
@@ -0,0 +1,24 @@
+document.querySelector("#demo").innerHTML = "Look the magic of querySelector in javaScript";
+
+
+document.querySelector(".trail").innerHTML = "Hello world";
+
+
+const heElement = document.querySelector('h1');
+heElement.textContent = 'I am becoming a rich and healthy person';
+heElement.style.color = 'red';
+
+
+const tackle = document.querySelector('.gold');
+tackle.textContent = 'You are an Amazing person Mr Govind Thakur';
+tackle.style.color = 'hotpink';
+
+
+const hello = document.querySelector('.hold');
+hello.textContent = "Learning about console methods console()";
+
+
+console.clear("claar all the console");
+console.log("Hello world");
+console.warn("Don't Do This"); // Look whole console methods in browser console
+console.error("TypeError");
